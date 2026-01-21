239bc8ae3c7f5635ba19220bb9b440dc70c1e52d
SROEUN Vathana <vathanasroeun@gmail.com>
2026-01-21 15:15:13 +0700

    add image

---
+++ src/main/java/com/acleda/model/Role.java
@@ -0,0 +1,70 @@
+package com.acleda.model;
+
+public class Role {
+	private int roleId;
+	private String roleName;
+	private String description;
+	private String createdAt;
+	private String createdBy;
+	private String updatedBy;
+	private String updatedAt;
+	private String deletedAt;
+	private String deletedBy;
+	
+	public int getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public String getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(String updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	public String getdeletedAt() {
+		return deletedAt;
+	}
+	public void setDeletedAt(String deletedAt) {
+		this.deletedAt = deletedAt;
+	}
+	public String getDeletedBy() {
+		return deletedBy;
+	}
+	public void setDeletedBy(String deletedBy) {
+		this.createdBy = deletedBy;
+	}
+	
+	
+}
